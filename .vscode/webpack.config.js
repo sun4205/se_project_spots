c240d21920ec22769c031fccebffb248586b0f0f
sun4205 <hanna4205@gmail.com>
2024-10-07 19:35:16 -0400

    webpack, babel

---
+++ .vscode/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+
+  mode: "development",
+  devtool: "inline-source-map",
+  stats: "errors-only",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+
+  // Babel related configuration
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+};
